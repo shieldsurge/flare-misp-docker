01f15af5c2f056df5190c20a5fb3a730603eae7e
shieldsurge <110939943+shieldsurge@users.noreply.github.com>
2022-08-31 20:37:20 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 # MISP configuration
 echo "Creating FLARE MISP integration configuration file"
 cd /opt/mtc/config/
+sed -i "s|stixtransclient.client.key\s*=\s*.*$|stixtransclient.client.key='$AIS_KEY_PATH'|" config.properties
+sed -i "s|stixtransclient.client.cert\s*=\s*.*$|stixtransclient.client.cert='$AIS_CERT_PATH'|" config.properties
+sed -i "s|stixtransclient.misp.key\s*=\s*.*$|stixtransclient.misp.key='$MISP_KEY'|" config.properties
 
 # Start supervisord
-echo "Starting supervisord"
-cd /
+echo "Starting supervisor
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
