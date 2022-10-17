e5e991367b3f27b82f97caa63f7c8b11784f2d48
shieldsurge <110939943+shieldsurge@users.noreply.github.com>
2022-10-16 23:23:42 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ cd /opt/mtc/config/
 sed -i "s|stixtransclient.client.key\s*=\s*.*$|stixtransclient.client.key='$AIS_KEY_PATH'|" config.properties
 sed -i "s|stixtransclient.client.cert\s*=\s*.*$|stixtransclient.client.cert='$AIS_CERT_PATH'|" config.properties
 sed -i "s|stixtransclient.misp.key\s*=\s*.*$|stixtransclient.misp.key='$MISP_KEY'|" config.properties
+sed -i "s|stixtransclient.source.collection\s*=\s*.*$|stixtransclient.source.collection='$COLLECTION'|" config.properties
 
 # Start the MISP FLARE service
 cd /opt/mtc/
