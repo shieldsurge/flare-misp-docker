084914c8971f9674b614126e11f00d60e6930c7d
shieldsurge <110939943+shieldsurge@users.noreply.github.com>
2022-10-18 00:45:03 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,8 +13,16 @@ echo "Creating FLARE MISP integration configuration file"
 cd /opt/mtc/config/
 sed -i "s|^stixtransclient.client.key\s*=\s*.*$|stixtransclient.client.key='$AIS_KEY_PATH'|" config.properties
 sed -i "s|^stixtransclient.client.cert\s*=\s*.*$|stixtransclient.client.cert='$AIS_CERT_PATH'|" config.properties
+sed -i "s|^client.ssl.key-store\s*=\s*.*$|client.ssl.key-store=file:'$AIS_P12_PATH'|" application.properties
+sed -i "s|^server.ssl.key-store\s*=\s*.*$|server.ssl.key-store=file:'$AIS_P12_PATH'|" application.properties
+sed -i "s|^server.ssl.key-store-password\s*=\s*.*$|server.ssl.key-store-password='$AIS_P12_PASS'|" application.properties
+sed -i "s|^server.ssl.key-password\s*=\s*.*$|server.ssl.key-password='$AIS_P12_PASS'|" application.properties
+sed -i "s|^misptransclient.post.baseurl\s*=\s*.*$|misptransclient.post.baseurl=https://$MISP_SERVER/events/|" config.properties
+sed -i "s|^stixtransclient.misp.url\s*=\s*.*$|stixtransclient.misp.url=https://$MISP_SERVER/health/|" config.properties
 sed -i "s|^stixtransclient.misp.key\s*=\s*.*$|stixtransclient.misp.key='$MISP_KEY'|" config.properties
 sed -i "s|^stixtransclient.source.collection\s*=\s*.*$|stixtransclient.source.collection=$COLLECTION|" config.properties
+sed -i "s|^mtc.quartz.frequency\s*=\s*.*$|mtc.quartz.frequency=$FREQUENCY|" config.properties
+sed -i "s|^rtimeout\s*=\s*.*$|rtimeout='$READ_TIMEOUT'|" application.properties
 
 # Start the MISP FLARE service
 cd /opt/mtc/
